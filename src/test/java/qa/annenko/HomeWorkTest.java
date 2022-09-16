eb2667ac44c958564bcb57bc0f786ab966080fd2
AnnaAnnenko <annenko.anya@gmail.com>
2022-09-16 09:37:48 +0300

    First test to Master

---
+++ src/test/java/qa/annenko/HomeWorkTest.java
@@ -0,0 +1,12 @@
+package qa.annenko;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HomeWorkTest {
+
+    @Test
+    void exampleTest() {
+        Assertions.assertTrue( 2 > 1);
+    }
+}
